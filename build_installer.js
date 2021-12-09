0789ff779deaa6f53614c6ff99b1b5b5fa9f6ce0
Spirit <spiritelectro.kontakt@gmail.com>
2021-12-09 22:24:24 +0100

    E

---
+++ build_installer.js
@@ -0,0 +1,38 @@
+// ./build_installer.js
+
+// 1. Import Modules
+const { MSICreator } = require('electron-wix-msi');
+const path = require('path');
+
+// 2. Define input and output directory.
+// Important: the directories must be absolute, not relative e.g
+// appDirectory: "C:\\Users\sdkca\Desktop\OurCodeWorld-win32-x64", 
+const APP_DIR = path.resolve(__dirname, './VALTracker-win32-x64');
+// outputDirectory: "C:\\Users\sdkca\Desktop\windows_installer", 
+const OUT_DIR = path.resolve(__dirname, './windows_installer');
+
+// 3. Instantiate the MSICreator
+const msiCreator = new MSICreator({
+    appDirectory: APP_DIR,
+    outputDirectory: OUT_DIR,
+
+    // Configure metadata
+    description: 'VALTracker Alpha V0.2',
+    exe: 'VALTracker',
+    name: 'VALTracker',
+    manufacturer: 'Spirit',
+    version: '0.0.2',
+    appIconPath: "./icons/Avatar.ico",
+
+    // Configure installer User Interface
+    ui: {
+        chooseDirectory: true
+    },
+});
+
+// 4. Create a .wxs template file
+msiCreator.create().then(function(){
+
+    // Step 5: Compile the template to a .msi file
+    msiCreator.compile();
+});
